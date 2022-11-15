58285adc9ae44e4f103a6a7fc504eae41a1e914f
Jeremias Gomez Giglio <72324512+JereGomez@users.noreply.github.com>
2022-11-15 11:44:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.get('/' , (req, res)=>{
     res.send("Hola desde servidor!!!");
 });
 
+app.get('/modif' , (req, res)=>{
+    res.send("Modificacion para deploy automatico");
+});
+
 const PORT = 8080;
 app.listen(PORT, ()=>{console.log(`Escuchando en el puerto ${PORT}`)})
